c69b30a50e83a1715cc8ab2a6280df94f6ba0650
sumanadithan <sumanadithang34@gmail.com>
2026-06-23 23:13:15 +0530

    feat: add github actions

--- documentation/next.config.mjs
+++ documentation/next.config.mjs
@@ -5,6 +5,13 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If you decide NOT to use a custom root domain and host on the default github URL
+  // (e.g. https://<username>.github.io/ai-agent), you MUST uncomment the line below:
+  // basePath: '/ai-agent',
 };
 
 export default withMDX(config);
